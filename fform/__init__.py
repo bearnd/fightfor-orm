10220fea39c56073e6c5fe0a2b9b1bbd22ab4500
Adamos Kyriakou <somada141@gmail.com>
2018-10-21 19:49:06 +1100

    Updated imports.

--- fform/__init__.py
+++ fform/__init__.py
@@ -12,3 +12,9 @@ from fform import orm_base
 from fform import orm_ct
 from fform import dal_base
 from fform import dals_ct
+from fform import orm_app
+from fform import dals_app
+from fform.orm_mt import *
+from fform.orm_ct import *
+from fform.orm_pubmed import *
+from fform.orm_app import *
