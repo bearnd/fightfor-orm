79028c2d18144a4582c534a74cd94e948c1f0409
Adamos Kyriakou <somada141@gmail.com>
2019-01-27 13:51:12 +1030

    Added unit-tests for the `TermThesaurusId` class as well as the `iodi_term_thesaurus_id` method of the `DalMesh` class.

--- tests/assets/items_mt.py
+++ tests/assets/items_mt.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from typing import Optional, Tuple, Dict
+from typing import Tuple, Dict
 
 from fform.orm_mt import Term
 from fform.dals_mt import DalMesh
@@ -38,5 +38,34 @@ def create_term(dal: DalMesh, **kwargs) -> Tuple[int, Dict]:
         setattr(obj, k, v)
 
     obj_id = dal.iodu_term(**refr)
+
+    return obj_id, refr
+
+
+def create_thesaurus_id(dal: DalMesh, **kwargs) -> Tuple[int, Dict]:
+    """ Inserts a new `thesaurus_ids` record.
+
+    Args:
+        dal (DalMesh): The DAL used to interact with the DB.
+
+    Returns:
+        Tuple(int, Dict):
+            - The PK ID of the new record.
+            - The inserted record reference.
+    """
+
+    refr = {
+        "thesaurus_id": "POPLINE (1978)",
+    }
+
+    # Override any reference pairs with values under `kwargs`.
+    for k, v in kwargs.items():
+        refr[k] = v
+
+    obj = Term()
+    for k, v in refr.items():
+        setattr(obj, k, v)
+
+    obj_id = dal.iodi_thesaurus_id(**refr)
 
     return obj_id, refr
