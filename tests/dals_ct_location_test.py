551cb23c06773b9eeb85f55f0d65f29e6d058372
Adamos Kyriakou <somada141@gmail.com>
2019-01-27 13:25:50 +1030

    Fixed incorrect method name.

--- tests/dals_ct_location_test.py
+++ tests/dals_ct_location_test.py
@@ -166,7 +166,7 @@ class DalCtLocationTest(DalCtTestBase):
 
         self.assertEqual(obj_id, 4)
 
-    def test_delete_person(self):
+    def test_delete_location(self):
         """Tests the deletion of a `Location` record via the `delete` method of
         the `DalClinicalTrials` class."""
 
