21e174b623f2a8f82bf5e0324fbb3aaf3937c75b
Adamos Kyriakou <somada141@gmail.com>
2019-10-24 17:04:31 +1030

    [ch1695]: Added `HealthTopic` class.
    [ch1695]: Added a new `HealthTopic` class representing `health-topic` XML elements.

--- fform/orm_mp.py
+++ fform/orm_mp.py
@@ -42,3 +42,61 @@ class HealthTopicGroup(Base, OrmFightForBase):
     url = sqlalchemy.Column(
         name="url", type_=sqlalchemy.types.UnicodeText(), nullable=False
     )
+class HealthTopic(Base, OrmFightForBase):
+    """ Table of `<health-topic>` element records representing a
+        health-topic.
+    """
+
+    # Set table name.
+    __tablename__ = "health_topics"
+
+    # Autoincrementing primary key ID.
+    health_topic_id = sqlalchemy.Column(
+        name="health_topic_id",
+        type_=sqlalchemy.types.Integer(),
+        primary_key=True,
+        autoincrement="auto",
+    )
+
+    # Referring to the `id` attribute.
+    ui = sqlalchemy.Column(
+        name="ui",
+        type_=sqlalchemy.types.Integer(),
+        nullable=False,
+        unique=True,
+        index=True,
+    )
+
+    # Referring to the `title` attribute.
+    title = sqlalchemy.Column(
+        name="title",
+        type_=sqlalchemy.types.Unicode(),
+        nullable=False,
+        unique=True,
+        index=True,
+    )
+
+    # Referring to the `url` attribute.
+    url = sqlalchemy.Column(
+        name="url", type_=sqlalchemy.types.UnicodeText(), nullable=False
+    )
+
+    # Referring to the `meta-desc` attribute.
+    description = sqlalchemy.Column(
+        name="description", type_=sqlalchemy.types.UnicodeText(), nullable=False
+    )
+
+    # Referring to the `<full-summary>` element.
+    summary = sqlalchemy.Column(
+        name="summary", type_=sqlalchemy.types.UnicodeText(), nullable=True
+    )
+
+    date_created = sqlalchemy.Column(
+        name="date_created", type_=sqlalchemy.types.Date(), nullable=False
+    )
+
+    # Set table arguments.
+    __table_args__ = {
+        # Set table schema.
+        "schema": "medline"
+    }
